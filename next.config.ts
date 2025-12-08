7bd8f3b9a02792e69c018638677573c406ef4323
Aryan chouhan <sfs.aryanchouhan24@gmail.com>
2025-12-08 16:36:13 +0530

    configeration fixed

--- next.config.ts
+++ next.config.ts
@@ -22,13 +22,25 @@
 // }
 
 // export default nextConfig
-import type { NextConfig } from 'next'
+// import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['lh3.googleusercontent.com'],
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['lh3.googleusercontent.com'],
+//   },
+// }
+
+// export default nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    FIREBASE_ADMIN_PROJECT_ID: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    FIREBASE_ADMIN_CLIENT_EMAIL: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
   },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
+
