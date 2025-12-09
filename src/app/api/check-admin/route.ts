dbecaa13927e90e49fc3996d3faf83c63ce74243
React-2025 <react-2025@React-2025s-Mac-mini.local>
2025-12-09 09:56:28 +0530

    routeprotector fixed

--- src/app/api/check-admin/route.ts
+++ src/app/api/check-admin/route.ts
@@ -1,10 +1,9 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
-
 import { NextResponse } from "next/server";
 import { getAuth } from "firebase-admin/auth";
 import { initAdmin } from "@/lib/firebaseAdmin";
+
 
 initAdmin();
 
@@ -15,9 +14,9 @@ export async function POST(req: Request) {
 
     // DEBUG LOGS (will show in terminal)
     console.log("Decoded email:", decoded.email);
-    console.log("ADMIN_EMAIL:", process.env.ADMIN_EMAIL);
+    console.log("ADMIN_EMAIL:", process.env.NEXT_PUBLIC_ADMIN_EMAIL);
 
-    const isAdmin = decoded.email === process.env.ADMIN_EMAIL;
+    const isAdmin = decoded.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL;
 
     return NextResponse.json({ isAdmin });
   } catch (error: any) {
